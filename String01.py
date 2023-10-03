fe48a9a19cc501d09ce767d2e91fd8309e359928
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-03 08:28:04 -0400

    Return the length1

--- String01.py
+++ String01.py
@@ -1,11 +1,11 @@
-def main(a):
+def main():
     """"Return an optional string of length eight
     Args:
         None
     Returns:
         str: return answer.
     """
+    a="12345678"
     if len(a)==8:
         return len(a),a
-print(main("12345678"))
-print(main("aaaazzzz"))
+print(main())
